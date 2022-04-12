8a2b530821fcbcf0535dbcf724f36af22b68f121
Andrew Smith <asmith@newstore.com>
2022-04-12 21:09:16 +0200

    add testing for password package to improve coverage

--- pkg/password/password_test.go
+++ pkg/password/password_test.go
@@ -1,1 +1,24 @@
 package password_test
+
+import (
+	"testing"
+
+	"github.com/robotlovesyou/fitest/pkg/password"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewWeakCreatesValidHashes(t *testing.T) {
+	pwd := "password"
+	nw := password.NewWeak()
+	hash, err := nw.Hash(pwd)
+	require.NoError(t, err)
+	require.True(t, nw.Compare(hash, pwd))
+}
+
+func TestNewCreatesValidHashes(t *testing.T) {
+	pwd := "password"
+	n := password.New()
+	hash, err := n.Hash(pwd)
+	require.NoError(t, err)
+	require.True(t, n.Compare(hash, pwd))
+}
